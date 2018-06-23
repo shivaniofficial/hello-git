eaa0f75257968d7576ac6ad9aabde1fb2994c056
shivani <shivanidhiman656@gmail.com>
2018-06-23 18:13:01 +0530

    Assignment-2

---
+++ Assignment-2.py
@@ -0,0 +1,28 @@
+# Assignment-2
+# question-1  the print statement in python is like.
+print("This is print statement in python")
+
+# question-2  joining two strings in python usin "+".
+str1 = "Acad"
+str2 = "View"
+str3 = str1 + str2
+print(str3)
+
+# question-3  take 3 inputs from user and print their values too.
+num1 = input("enter the first value : ")
+num2 = input("enter the second value : ")
+num3 = input("enter the third value : ")
+print("first value : " + str(num1))
+print("second value : " + str(num2))
+print("third value : " + str(num3))
+
+# question-4  print "Let's get started"
+print("\"Let's get started\"")
+
+# question-5  print the values using placeholders
+print("s=%s \ncourse=%s \nfees=%d" %("\"Acadview\"","\"Python\"",5000))
+
+#question-6 exercise :
+name = "Tony Stark"
+salary = 1000000
+print('%s"%d')%(name,salary)
